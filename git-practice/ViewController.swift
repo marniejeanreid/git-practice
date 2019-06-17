491fe3a2dbe9b3b8d5f48dd5f5b46a57277d1a20
Marnie Reid <marniejeanreid@gmail.com>
2019-06-17 13:58:57 -0400

    Initial Commit

---
+++ git-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git-practice
+//
+//  Created by Marnie Reid on 6/17/19.
+//  Copyright © 2019 Marnie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
